ab86c0b218c4f6226d07679e28ec0962a83c0cc8
Bsilvs95 <brian9550@gmail.com>
2019-11-23 10:33:35 -0500

    homework orderbook

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,34 @@
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+  // If list is empty just add the order and return
+
+  let updatedBook = []
+
+  if (existingBook.length === 0) {
+    existingBook.push(incomingOrder)
+    return existingBook
+  }
+
+  if ((existingBook[0].type === 'buy' && incomingOrder.type === 'sell') && (existingBook[0].quantity === incomingOrder.quantity) && (existingBook[0].price === incomingOrder.price)) {
+    return updatedBook
+  }
+
+  else if ((existingBook[0].type === 'buy' && incomingOrder.type === 'sell') && (existingBook[0].quantity > incomingOrder.quantity) && (existingBook[0].price === incomingOrder.price)) {
+    const updatedQuantity = existingBook[0].quantity - incomingOrder.quantity
+    existingBook[0].quantity = updatedQuantity
+    return existingBook
+  }
+
+  else if ((existingBook[0].type === 'buy' && incomingOrder.type === 'sell') && (existingBook[0].quantity < incomingOrder.quantity) && (existingBook[0].price === incomingOrder.price)) {
+    let partialOrder = []
+    const remainderQuantity = incomingOrder.quantity - existingBook[0].quantity
+    incomingOrder.quantity = remainderQuantity
+    partialOrder.push(incomingOrder)
+    return partialOrder
+  }
+  else {
+    existingBook.push(incomingOrder)
+    return existingBook
+  }
 }
 
 module.exports = reconcileOrder
